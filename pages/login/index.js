f1e39ef5dc1268edf2d9c35c3e0f379f5d8f1c21
daytoy <411487840@qq.com>
2020-07-20 01:28:02 +0800

    add3

--- pages/login/index.js
+++ pages/login/index.js
@@ -10,47 +10,30 @@ Page({
   login() {
     wx.login({
       success: res => {
-        
-        if (res.code) {
-          let code = res.code;
-         
-          // 获取用户信息
-          wx.getSetting({
-            success: ures => {
-              if (ures.authSetting['scope.userInfo']) {
-                // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-                wx.getUserInfo({
-                  success: user => {
-                    wx.setStorageSync("userInfo", user);
-                    console.log(user);
-                    
-                    if (this.userInfoReadyCallback) {
-                      this.userInfoReadyCallback(user)
-                    }
-                 
-                    wx.request({
-                      url: "http://111.230.173.74:7008/thread/getOpenid/",
-                      data: {
-                        code: JSON.stringify(code)
-                      },
-                      method: "get",
-                      header: {
-                        'Content-Type': 'application/json'
-                      },
-                      success: function (res) {
-                        console.log("登录返回的数据：");
-                        console.log(res.data);
-                      },
-                      fail: function (error) {
-                        console.log(error);
-                      }
-                    })
-                  }
-                })
-              }
+        wx.getUserInfo({
+          success: user => {
+            wx.setStorageSync("userInfo", user);
+            console.log(user);
+
+            if (this.userInfoReadyCallback) {
+              this.userInfoReadyCallback(user)
             }
-          })
-        }
+
+            wx.request({
+              url: "http://111.230.173.74:7008/thread/getOpenid/",
+              data: {
+                code: res.code
+              },
+              header: {
+                "content-type": "application/json"
+              },
+              method: "get",
+              success: function (e) {
+                wx.setStorageSync("openid", e.data.openid)
+              }
+            })
+          }
+        })
       }
     })
   }
